3684b63b2e186be570477fafc949d4c73425a01f
ct-school <ctrudeau@csuchico.edu>
2025-05-05 08:36:52 -0700

    Adding first round of unit tests for guesser class

--- GuesserTest.cpp
+++ GuesserTest.cpp
@@ -17,6 +17,26 @@ class GuesserTest : public ::testing::Test
 // Example "smoke test" (can be deleted)
 TEST(GuesserTest, smoke_test)
 {
-  Guesser object("Secret");
+  Guesser object0("Secret");
   ASSERT_EQ( 1+1, 2 );
+  // test if entering a match more than 3 times throws an error
+  EXPECT_EQ(1, object0.match("Secret"));
+  EXPECT_EQ(1, object0.match("Secret"));
+  EXPECT_EQ(1, object0.match("Secret"));
+  EXPECT_EQ(1, object0.match("Secret"));
+  // test if it gets reset
+  Guesser object1("Secret");
+  EXPECT_EQ(0, object1.match("secret"));
+  EXPECT_EQ(0, object1.match("secret"));
+  EXPECT_EQ(1, object1.match("Secret"));
+  EXPECT_EQ(0, object1.match("secret"));
+  EXPECT_EQ(1, object1.match("Secret"));
+  // above test fails test if its a brute force issue
+  Guesser object2("Secret");
+  EXPECT_EQ(0, object2.match("Secrer"));
+  EXPECT_EQ(0, object2.match("secret"));
+  EXPECT_EQ(1, object2.match("Secret"));
+  EXPECT_EQ(0, object2.match("secret"));
+  EXPECT_EQ(1, object2.match("Secret"));
+
 }
